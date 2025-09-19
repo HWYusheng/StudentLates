4190de4a1c32c87292728b5270ed31b34f6dcbe3
235274 <235274@B18-16>
2025-09-19 15:14:48 +0100

    Add project files.

---
+++ StudentLates/Late.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLates
+{
+    public class Late
+    {
+        public int LateID { get; set; }
+        public int StudentID { get; set; }
+        public DateTime DateOfLate { get; set; }
+        public int Period { get; set; }
+        public int MinsLate { get; set; }
+    }
+}
---
+++ StudentLates/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLates
+{
+    public class Student
+    {
+        public int StudentID { get; set; } // property for Student ID
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StudentDOB { get; set; }
+        public string FullName // used for combobox display 
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+    }
+}
